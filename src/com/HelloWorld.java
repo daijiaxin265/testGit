a3aacfc4d63c4fb23386c3275529f20e7de2313b
daijiixin666 <2654998479@qq.com>
2020-10-29 21:30:59 +0800

    更新一

--- src/com/HelloWorld.java
+++ src/com/HelloWorld.java
@@ -2,7 +2,8 @@ package com;
 
 public class HelloWorld {
 	public static void main(String[]args){
-		System.out.println("helloworld!");
+		System.out.println("helloworld Git!");
+		System.out.println("更新");
 	}
 
 }
